14ae9d3f03016e401960cf698187b81e3bd2064c
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2021-08-10 16:12:37 +0900

    KVOパターンにnavigationControllerを追加

--- WKWebView-KVO/WKWebView-KVO/SceneDelegate.swift
+++ WKWebView-KVO/WKWebView-KVO/SceneDelegate.swift
@@ -15,7 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = WKWebViewController()
+        let navigationController = UINavigationController(rootViewController: WKWebViewController())
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         guard let _ = (scene as? UIWindowScene) else { return }
     }
